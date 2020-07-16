cee79d763d43a5e6469d7223ac23adc43d099e7e
songxiaoa <15778597023@163.com>
2020-07-17 01:46:33 +0800

    添加轮播图的样式，添加首页的交互

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var num = 0;
+var text = '';
+for (var i = 0; i < 4; i++) {
+    num = i
+    $('#plate' + num).bind('click', tab)
+}
+
+function tab(e) {
+    $('.plate-right li div').not(tab).removeClass('plate-active');
+    var target = e.currentTarget;
+    var tab = $($(target).find("div").get(0))
+    text = tab.text();
+    tab.addClass('plate-active');
+   
+    $('.m-b div').text(text)
+}
