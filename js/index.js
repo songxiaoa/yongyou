bec2361de13c5ac940d35f57fa6509491e75c1f4
zhaiguolong <tigerfpv@gmail.com>
2020-07-19 21:29:23 +0800

    优化专题样式，适配腾讯VID 视频播放器

--- js/index.js
+++ js/index.js
@@ -5,20 +5,24 @@ for (var i = 0; i < 4; i++) {
     $('#plate' + num).bind('click', tab)
 }
 var cont = (
-    `<video width="640" height="360" controls  poster="./images/tu_gaofengduihua.png">
-        <source src="./images/video/movie.ogg"  type="video/mp4">
-        <source src="./images/video/movie.ogg"  type="video/ogg">
-        您的浏览器不支持 HTML5 video 标签。
-    </video>`
+    `<div id="c_video_container"></div>`
 )
 function tab(e) {
     $('.plate-right li div').not(tab).removeClass('plate-active');
     var target = e.currentTarget;
     var tab = $($(target).find("div").get(0))
-    text = tab.text();
+    //text = tab.text();
+	var vid = target.dataset.vid;
     tab.addClass('plate-active');
     // $('.m-b div').text(text)
-    $('.plate-cont').html(cont)
-        
-    
+    //$('.plate-cont').html(cont);
+	player1.play({vid: vid})
 }
+
+
+var player1 = new Txplayer({
+  containerId: 'c_video_container',
+  vid: 'j3117wy6h8p',
+  width: '640px',
+  height: '360px'
+});
